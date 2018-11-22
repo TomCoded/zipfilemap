58462e114f28db5757a6e4bc3a80f6efdfb781e8
Tom White <tomtinkerer@gmail.com>
2018-11-22 10:30:07 -0800

    linting

--- test/files.js
+++ test/files.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 const assert = require('assert');
 const path = require('path');
@@ -11,33 +11,32 @@ let zipBuffer;
 
 beforeEach('', () => {
     zipBuffer = fs.readFileSync(filePath);
-})
+});
 
 describe('local file tests', () => {
-
     it('should unzip local file to memory', async () => {
-	let dictOfFiles = await zipfilemap.fromBuffer(zipBuffer);
-	assert(dictOfFiles);
+        const dictOfFiles = await zipfilemap.fromBuffer(zipBuffer);
+        assert(dictOfFiles);
     });
 
     it('should unzip local file with the right number of entries', async () => {
-	let dictOfFiles = await zipfilemap.fromBuffer(zipBuffer);
-	assert.strictEqual(Object.keys(dictOfFiles).length,filesInZip.length);
+        const dictOfFiles = await zipfilemap.fromBuffer(zipBuffer);
+        assert.strictEqual(Object.keys(dictOfFiles).length, filesInZip.length);
     });
 
     it('should unzip local file to entries that match zipped files', async () => {
-	let dictOfFiles = await zipfilemap.fromBuffer(zipBuffer);
-	Object.values(filesInZip).forEach((filename)=>{
-	    assert(filename in dictOfFiles);
-	})
+        const dictOfFiles = await zipfilemap.fromBuffer(zipBuffer);
+        Object.values(filesInZip).forEach((filename) => {
+            assert(filename in dictOfFiles);
+        });
     });
 
     it('should have matching contents between zipped files and originals', async () => {
-	let dictOfFiles = await zipfilemap.fromBuffer(zipBuffer);
-	Object.values(filesInZip).forEach((filename)=>{
-	    let originalFile = fs.readFileSync(path.join(__dirname, 'fixtures', filename));
-	    let originalFileData = originalFile.toString();
-	    assert(originalFileData === dictOfFiles[filename]);
-	})
+        const dictOfFiles = await zipfilemap.fromBuffer(zipBuffer);
+        Object.values(filesInZip).forEach((filename) => {
+            const originalFile = fs.readFileSync(path.join(__dirname, 'fixtures', filename));
+            const originalFileData = originalFile.toString();
+            assert(originalFileData === dictOfFiles[filename]);
+        });
     });
 });
